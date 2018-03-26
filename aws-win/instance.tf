e3eff06b206ec9eb85e54537bed034c69ad467bc
Dan Iverson <dan@daniverson.com>
2018-03-26 13:11:41 -0600

    Remove wide-open security rules

--- aws-win/instance.tf
+++ aws-win/instance.tf
@@ -127,22 +127,6 @@ resource "aws_security_group" "ps-terraform-win" {
     name = "ps-terraform_${var.platform}"
     description = "ps-terraform windows internal traffic + maintenance."
 
-    // These are for internal traffic
-    ingress {
-        from_port = 0
-        to_port = 65535
-        protocol = "tcp"
-        self = true
-    }
-
-    ingress {
-        from_port = 0
-        to_port = 65535
-        protocol = "udp"
-        self = true
-    }
-
-    // These are for maintenance
     ingress {
         from_port = 5985
         to_port = 5986
